857c85ecfa6611b70a22697641f8f54c35adb509
Marta Marchiori Manerba <65548984+MartaMarchiori@users.noreply.github.com>
2021-12-22 09:54:30 +0100

    Add files via upload

---
+++ FairnessEval.py
@@ -0,0 +1,181 @@
+from Imports import *
+
+clfsNames=['Orig','OrigBlind','Mit','MitBlind']
+
+def print_fairness(metric_name, metric_matrix, protected_feature, bin_names):
+    res=[]
+    """Prints out which sub-populations violate a group fairness metric."""
+    print('The *{}* group-based fairness metric for *{}* feature split '
+          'are:'.format(metric_name, protected_feature))
+    for grouping_i, grouping_name_i in enumerate(bin_names):
+        j_offset = grouping_i + 1
+        for grouping_j, grouping_name_j in enumerate(bin_names[j_offset:]):
+            grouping_j += j_offset
+            if metric_matrix[grouping_i, grouping_j]:
+                is_not = ' NOT '  
+                res.append(grouping_name_i+grouping_name_j)
+                print('    * {} satisfied for "{}" and "{}" '
+                      'sub-populations.'.format(is_not, grouping_name_i,
+                                                grouping_name_j))
+    return res
+
+def differences(list1,list2):
+    list1=clean(list1)
+    list2=clean(list2)
+    set_difference = set(list1).symmetric_difference(set(list2))
+    list_difference = list(set_difference)
+    return list_difference
+    
+def clean(disparateList):
+    res=[]
+    char_to_replace = {
+                       '"': '',
+                       "''": '',
+                       ',': ' ',
+                       '(': '',
+                       ')': ''
+                                }
+    for item in disparateList:
+        sample_string=item
+    # Iterate over all key-value pairs in dictionary
+        for key, value in char_to_replace.items():
+            # Replace key character with value character in string
+            sample_string = sample_string.replace(key, value)
+        res.append(sample_string)
+    return res
+
+def DataDescription(X_test,y_test,class_names,column_names,protected_values,protected_feature):
+    res=pd.DataFrame(X_test, columns=column_names)
+    # Select a protected feature
+    protected_feature = protected_feature
+
+    res[protected_feature]=res[protected_values].idxmax(axis=1)
+    FATInput = np.array(res[[protected_feature]].to_records(index=False), dtype=[(protected_feature, '<U113')])
+
+    # Group the data based on the unique values of the column
+    class_names=class_names
+    grouping_column = protected_feature
+    grouping_indices, grouping_names = fatf_data_tools.group_by_column(
+        FATInput, grouping_column, treat_as_categorical=True)
+
+    # Print out the data distribution for the grouping
+    print('The grouping based on the *{}* feature has the '
+        'following distribution:'.format(grouping_column))
+    for grouping_name, grouping_idx in zip(grouping_names, grouping_indices):
+        print('    * "{}" grouping has {} instances.'.format(
+            grouping_name, len(grouping_idx)))
+
+    # Get the class distribution for each sub-grouping
+    grouping_class_distribution = dict()
+    for grouping_name, grouping_idx in zip(grouping_names, grouping_indices):
+        sg_y = y_test[grouping_idx]
+        sg_classes, sg_counts = np.unique(sg_y, return_counts=True)
+
+        grouping_class_distribution[grouping_name] = dict()
+        for sg_class, sg_count in zip(sg_classes, sg_counts):
+            sg_class_name = class_names[sg_class]
+
+            grouping_class_distribution[grouping_name][sg_class_name] = sg_count
+
+    # Print out the class distribution per sub-group
+    print('\nThe class distribution per sub-population:')
+    for grouping_name, class_distribution in grouping_class_distribution.items():
+        print('    * For the "{}" grouping the classes are distributed as '
+            'follows:'.format(grouping_name))
+        for class_name, class_count in class_distribution.items():
+            print('        - The class *{}* has {} data points.'.format(
+                class_name, class_count))
+    return FATInput
+
+def disparateImpactMetrics(clfs,FATInput,X_test,X_test_blind,y_test,protected_feature):
+    ea={}
+    eo={}
+    dp={}
+    i=0
+    for k, v in clfs.items():
+#   for i in range(len(clfs)):
+        clf=v#clfs[i]
+        if k=='clfOrig' or k=='clfMit':
+            y_pred=clf.predict(X_test)
+        else:
+            y_pred=clf.predict(X_test_blind)
+        target='target'
+        confusion_matrix_per_bin, bin_names = fatf_mt.confusion_matrix_per_subgroup(FATInput, y_test, y_pred, protected_feature, treat_as_categorical=True)
+        equal_accuracy_matrix = fatf_mfm.equal_accuracy(confusion_matrix_per_bin)
+        eaTemp = print_fairness('Equal Accuracy', equal_accuracy_matrix, protected_feature, bin_names)
+        equal_opportunity_matrix = fatf_mfm.equal_opportunity(confusion_matrix_per_bin)
+        eoTemp = print_fairness('Equal Opportunity', equal_opportunity_matrix, protected_feature, bin_names)
+        demographic_parity_matrix = fatf_mfm.demographic_parity(confusion_matrix_per_bin)
+        dpTemp = print_fairness('Demographic Parity', demographic_parity_matrix, protected_feature, bin_names)
+        ea[clfsNames[i]]=eaTemp
+        eo[clfsNames[i]]=eoTemp
+        dp[clfsNames[i]]=dpTemp
+        i+=1
+    resEA = computeDeltas(ea)
+    resEO = computeDeltas(eo)
+    resDP = computeDeltas(dp)
+    resDisparateImp = {'Equal Accuracy':resEA, 'Equal Opportunity':resEO, 'Demographic Parity':resDP}
+    return resDisparateImp
+
+def computeDeltas(dm):
+    res={}
+    res['Delta O-OB'] = differences(clean(dm['Orig']),clean(dm['OrigBlind']))
+    res['Delta M-MB'] = differences(clean(dm['Mit']),clean(dm['MitBlind']))
+    res['Delta O-M'] = differences(clean(dm['Orig']),clean(dm['Mit']))
+    res['Delta OB-MB'] = differences(clean(dm['OrigBlind']),clean(dm['MitBlind']))
+    return res
+
+def standardMetricsPerGroup(clfs,column_names,target,X_test,y_test,protected_feature,protected_values):
+    def predict_blind(X):
+        return clf.predict(X.drop(protected_values, axis=1))
+    
+    res=pd.DataFrame(X_test, columns=column_names)
+    res = res.assign(target=y_test)
+    res[protected_feature]=res[protected_values].idxmax(axis=1)
+    gb = res.groupby(protected_feature)    
+    by_protected_attr = [gb.get_group(x) for x in gb.groups]
+    metric_scores=[]
+    target='target'
+    for k, v in clfs.items():
+    #for i in range(len(clfs)):
+        precisions={}
+        recalls={}
+        #selection_rates={}
+        clf=v#clfs[i]
+        for i in range(len(by_protected_attr)):
+            y_test_r = by_protected_attr[i][target].values 
+            X_test_r = by_protected_attr[i].drop([target,protected_feature], 1)
+            if k=='clfOrig' or k=='clfMit': #if clf==clfOrig or clf==clfMit:
+                y_pred=clf.predict(X_test_r)
+            else:
+                y_pred=predict_blind(X_test_r)
+            precisions[protected_values[i]]=precision_score(y_pred, y_test_r, average='macro')
+            recalls[protected_values[i]]=recall_score(y_pred, y_test_r, average='macro')
+            #selection_rates[protected_values[i]]=selection_rate(y_pred, y_test_r, pos_label=0)
+        metric_score={
+            'Precision':precisions,
+            'Recall':recalls,
+            #'Selection Rate':selection_rates
+        }
+        metric_scores.append(metric_score) #['Orig','OrigBlind','Mit','MitBlind']
+
+    # https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+    for m in ['Precision','Recall']:#,'Selection Rate']:
+        labels = protected_values
+
+        x = np.arange(len(protected_values))  # the label locations
+        
+        fig, ax = plt.subplots()
+        rects1 = ax.plot(list(metric_scores[0][m].values()), linestyle='solid', label='Orig')
+        rects2 = ax.plot(list(metric_scores[1][m].values()), linestyle='dotted', label='OrigBlind')
+        rects3 = ax.plot(list(metric_scores[2][m].values()), linestyle='dashed', label='Mit')
+        rects4 = ax.plot(list(metric_scores[3][m].values()), linestyle='dashdot', label='MitBlind')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel(m)
+        ax.set_title(m)
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels,rotation=40,ha='right')
+        ax.legend()
+
+        plt.show()
---
+++ Rankers.py
@@ -0,0 +1,161 @@
+from Imports import *
+           
+# https://towardsdatascience.com/machine-learning-classifiers-comparison-with-python-33149aecdbca
+def rankers_evaluation(ListOfRankers,RankersNames,X,y,folds):
+    # Perform cross-validation to each machine learning classifier
+    CrossValidated=[]
+    for i in range(len(ListOfRankers)):
+        CrossValidated.append(cross_validate(ListOfRankers[i], X, y, cv=folds, scoring=scoring))
+    # Create a data frame with the models perfoamnce metrics scores
+    d={}
+    for i in range(len(CrossValidated)):
+        l=[ 
+            CrossValidated[i]['test_accuracy'].mean(),
+            CrossValidated[i]['test_precision'].mean(),
+            CrossValidated[i]['test_recall'].mean(),
+            CrossValidated[i]['test_f1_score'].mean()
+        ]
+        d[RankersNames[i]]=l
+    models_scores_table = pd.DataFrame(d,index=['Accuracy', 'Precision', 'Recall', 'F1 Score'])
+    # Return models performance metrics scores data frame
+    return models_scores_table
+
+def rankers(X,X_blind,Y):
+    print('Non-Blind Rankers')
+    gnb = GaussianNB()
+    param_grid = {'var_smoothing': np.logspace(0,-9, num=100)}
+    clf = RandomizedSearchCV(gnb, param_grid, random_state=0)
+    search = clf.fit(X, Y)
+    gnb = search.best_estimator_
+    print(gnb)
+
+    gnbCalibrated = CalibratedClassifierCV(base_estimator=gnb) #For prediction, the base estimator, trained using all the data, is used
+    gnbCalibrated.fit(X, Y)
+
+    lr=LogisticRegression()
+    param_grid = {'penalty' : ['l1', 'l2'], 'C' : np.logspace(-4, 4, 20), 'solver' : ['liblinear']}
+    clf = RandomizedSearchCV(lr, param_grid, random_state=0)
+    search = clf.fit(X, Y)
+    lr = search.best_estimator_
+    print(lr)
+
+    lrCalibrated = CalibratedClassifierCV(base_estimator=lr) #For prediction, the base estimator, trained using all the data, is used
+    lrCalibrated.fit(X, Y)
+
+    rf = RandomForestClassifier()
+    param_grid = {'n_estimators': [200, 500], 'max_features': ['auto', 'sqrt', 'log2'], 'max_depth' : [4,5,6,7,8], 'criterion' : ['gini', 'entropy'], 'bootstrap' : [True, False]}
+    clf = RandomizedSearchCV(rf, param_grid, random_state=0)
+    search = clf.fit(X, Y)
+    rf = search.best_estimator_
+    print(rf)
+
+    rfCalibrated = CalibratedClassifierCV(base_estimator=rf) #For prediction, the base estimator, trained using all the data, is used
+    rfCalibrated.fit(X, Y)
+
+    svc = SVC()
+    param_grid = {'C': [0.1,1], 'gamma': [1,0.1], 'kernel': ['linear', 'rbf']}#, 'poly', 'sigmoid']}
+    clf = RandomizedSearchCV(svc, param_grid, random_state=0)
+    search = clf.fit(X, Y)
+    search.best_params_['probability']=True
+    params = search.best_params_
+    svc = SVC(**params)
+    print(svc)
+
+    svcCalibrated = CalibratedClassifierCV(base_estimator=svc) #For prediction, the base estimator, trained using all the data, is used
+    svcCalibrated.fit(X, Y)
+
+    ListOfRankers=[
+        gnb,
+        gnbCalibrated,
+        lr,
+        lrCalibrated,
+        rf,
+        rfCalibrated,
+        svc,
+        svcCalibrated
+    ]
+    RankersNames=['Gaussian Naive Bayes','Calibrated Gaussian Naive Bayes','Logistic Regression',
+                       'Calibrated Logistic Regression','Random Forest','Calibrated Random Forest',
+                       'Support Vector Classifier','Calibrated Support Vector Classifier']
+    print('Non-Blind Rankers Evaluation')
+    m_e_not_blind = rankers_evaluation(ListOfRankers,RankersNames,X,Y,5)
+    print(m_e_not_blind)
+
+    print('Blind Rankers')
+    gnbBlind = GaussianNB()
+    param_grid = {'var_smoothing': np.logspace(0,-9, num=100)}
+    clf = RandomizedSearchCV(gnbBlind, param_grid, random_state=0)
+    search = clf.fit(X_blind, Y)
+    gnbBlind = search.best_estimator_
+    print(gnbBlind)
+
+    gnbCalibratedBlind = CalibratedClassifierCV(base_estimator=gnbBlind) #For prediction, the base estimator, trained using all the data, is used
+    gnbCalibratedBlind.fit(X_blind, Y)
+
+    lrBlind=LogisticRegression()
+    param_grid = {'penalty' : ['l1', 'l2'], 'C' : np.logspace(-4, 4, 20), 'solver' : ['liblinear']}
+    clf = RandomizedSearchCV(lrBlind, param_grid, random_state=0)
+    search = clf.fit(X_blind, Y)
+    lrBlind = search.best_estimator_
+    print(lrBlind)
+
+    lrCalibratedBlind = CalibratedClassifierCV(base_estimator=lrBlind) #For prediction, the base estimator, trained using all the data, is used
+    lrCalibratedBlind.fit(X_blind, Y)
+
+    rfBlind = RandomForestClassifier()
+    param_grid = {'n_estimators': [200, 500], 'max_features': ['auto', 'sqrt', 'log2'], 'max_depth' : [4,5,6,7,8], 'criterion' : ['gini', 'entropy'], 'bootstrap' : [True, False]}
+    clf = RandomizedSearchCV(rfBlind, param_grid, random_state=0)
+    search = clf.fit(X_blind, Y)
+    rfBlind = search.best_estimator_
+    print(rfBlind)
+
+    rfCalibratedBlind = CalibratedClassifierCV(base_estimator=rfBlind) #For prediction, the base estimator, trained using all the data, is used
+    rfCalibratedBlind.fit(X_blind, Y)
+
+    svcBlind = SVC()
+    param_grid = {'C': [0.1,1], 'gamma': [1,0.1], 'kernel': ['linear', 'rbf']}#, 'poly', 'sigmoid']}
+    clf = RandomizedSearchCV(svcBlind, param_grid, random_state=0)
+    search = clf.fit(X_blind, Y)
+    search.best_params_['probability']=True
+    params = search.best_params_
+    svcBlind = SVC(**params)
+    print(svcBlind)
+
+    svcCalibratedBlind = CalibratedClassifierCV(base_estimator=svcBlind) #For prediction, the base estimator, trained using all the data, is used
+    svcCalibratedBlind.fit(X_blind, Y)
+
+    ListOfRankersBlind=[
+        gnbBlind,
+        gnbCalibratedBlind,
+        lrBlind,
+        lrCalibratedBlind,
+        rfBlind,
+        rfCalibratedBlind,
+        svcBlind,
+        svcCalibratedBlind
+    ]
+    RankersNamesBlind=['Blind Gaussian Naive Bayes','Blind Calibrated Gaussian Naive Bayes','Blind Logistic Regression',
+                  'Blind Calibrated Logistic Regression','Blind Random Forest','Blind Calibrated Random Forest',
+                  'Blind Support Vector Classifier','Blind Calibrated Support Vector Classifier']
+    print('Blind Rankers Evaluation')
+    m_e_blind = rankers_evaluation(ListOfRankersBlind,RankersNamesBlind,X_blind,Y,5)
+    print(m_e_blind) 
+
+    frames = [m_e_not_blind,m_e_blind]
+    m_e = pd.concat(frames, axis=1)
+    m_e['Best Score'] = m_e.idxmax(axis=1)
+    print('Final Rankers Evaluation')
+    print(m_e)
+    ListOfAllRankers=ListOfRankers+ListOfRankersBlind
+    ListOfAllRankersNames=RankersNames+RankersNamesBlind
+    AllRankers={}
+    for i in range(len(ListOfAllRankers)):
+        AllRankers[ListOfAllRankers[i]]=ListOfAllRankersNames[i]
+    #AllRankers={gnb: 'Gaussian Naive Bayes', gnbCalibrated: 'Calibrated Gaussian Naive Bayes', lr: 'Logistic Regression', lrCalibrated: 'Calibrated Logistic Regression', rf: 'Random Forest', rfCalibrated: 'Calibrated Random Forest', svc: 'Support Vector Classifier', svcCalibrated: 'Calibrated Support Vector Classifier',
+    #            gnbBlind: 'Blind Gaussian Naive Bayes', gnbCalibratedBlind: 'Blind Calibrated Gaussian Naive Bayes', lrBlind: 'Blind Logistic Regression', lrCalibratedBlind: 'Blind Calibrated Logistic Regression', rfBlind: 'Blind Random Forest', rfCalibratedBlind: 'Blind Calibrated Random Forest', svcBlind: 'Blind Support Vector Classifier', svcCalibratedBlind: 'Blind Calibrated Support Vector Classifier'}
+    new_ke_lis = list(AllRankers.keys())
+    new_val = list(AllRankers.values())
+    new_pos = new_val.index(m_e['Best Score'].mode()[0]) # value from dictionary
+    ranker = new_ke_lis[new_pos]
+    print('------> Best Ranker: ',m_e['Best Score'].mode()[0])
+    return ranker
